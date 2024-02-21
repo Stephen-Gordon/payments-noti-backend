d62047ec2781e2b65ed96a4f7258aec94a620589
Stephen Gordon <n00202054@iadt.ie>
2024-02-21 15:24:36 +0000

    added console lo

--- server.js
+++ server.js
@@ -1,87 +1,87 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const host = "0.0.0.0";
 const path = require("path");
 const socketIO = require("socket.io");
 const fetch = require("node-fetch");
 const port = process.env.PORT || 3002;
 
+const app = express();
 
-const app = express()
-    app.use(express.static(path.join(__dirname, "public")))
-	app.use(express.json())
-	.post("/alchemyhook", (req, res) => {
-		notificationReceived(req);
-		res.status(200).end();
-	})
-	.get("/*", (req, res) => res.sendFile(path.join(__dirname + "/index.html")))
-	.listen(port, () => console.log(`Listening on ${port}`));
+// Middleware
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(cors());
 
-// start the websocket server
-const io = socketIO(app);
-
-// listen for client connections/calls on the WebSocket server
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('register address', (msg) => {
-    //send address to Alchemy to add to notification
-    addAddress(msg);
-  });
+// Routes
+app.post("/alchemyhook", (req, res) => {
+	notificationReceived(req);
+	res.status(200).end();
 });
 
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
-// notification received from Alchemy from the webhook. Let the clients know.
+// WebSocket server
+const server = app.listen(port, () => {
+	console.log(`Listening on ${port}`);
+});
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+	console.log("Client connected");
+	socket.on("disconnect", () => console.log("Client disconnected"));
+	socket.on("register address", (msg) => {
+		addAddress(msg);
+	});
+});
+
+// Notification received from Alchemy from the webhook. Let the clients know.
 const notificationReceived = async (req, res) => {
-  console.log("notification received!");
-  console.log("event hereeeeeeeeee", req?.body?.event?.activity[0]?.toAddress);
+	console.log("Notification received!");
+	console.log("Event:", req?.body?.event?.activity[0]?.toAddress);
+	io.emit("notification", JSON.stringify(req.body));
+};
 
-
-  io.emit('notification', JSON.stringify(req.body));
-
-}
-
-// add an address to a notification in Alchemy
+// Add an address to a notification in Alchemy
 async function addAddress(new_address) {
-  console.log("adding address " + new_address);
-  const body = { webhook_id: 'wh_eddaxijt09kqs3zh', addresses_to_add: [new_address], addresses_to_remove: [] };
-  try {
-    fetch("https://dashboard.alchemyapi.io/api/update-webhook-addresses", {
-			method: "PATCH",
-			body: JSON.stringify(body),
-			headers: { "Content-Type": "application/json" },
-			headers: { "X-Alchemy-Token": "2CxWNMpNMB3IeHKVJBROFv-19LQ3LKKQ" },
-		})
-			.then((res) => res.json())
-			.then((json) => console.log("Successfully added address:", json))
-			.catch((err) => console.log("Error! Unable to add address:", err));
-  }
-  catch (err) {
-    console.error(err);
-  }
+	console.log("Adding address:", new_address);
+	const body = {
+		webhook_id: "wh_eddaxijt09kqs3zh",
+		addresses_to_add: [new_address],
+		addresses_to_remove: [],
+	};
+	try {
+		const response = await fetch(
+			"https://dashboard.alchemyapi.io/api/update-webhook-addresses",
+			{
+				method: "PATCH",
+				body: JSON.stringify(body),
+				headers: {
+					"Content-Type": "application/json",
+					"X-Alchemy-Token": "2CxWNMpNMB3IeHKVJBROFv-19LQ3LKKQ",
+				},
+			}
+		);
+		const json = await response.json();
+		console.log("Successfully added address:", json);
+	} catch (err) {
+		console.error("Error! Unable to add address:", err);
+	}
 }
 
+require("dotenv").config();
+require("./configs/db.js")();
 
-require('dotenv').config();
-require('./configs/db.js')()
-app.use(cors());
-app.use(express.json())
-
-app.set('view engine', 'html')
-// serve the views 
-app.use(express.static(__dirname + '/views'))
-
+// Additional configurations
+app.set("view engine", "html");
+app.use(express.static(path.join(__dirname, "views")));
 
 app.use((req, res, next) => {
-    console.log(req.user)
-    return next()
-})
-/* Requests */
+	console.log(req.user);
+	return next();
+});
 
+app.use("/api/users", require("./routes/users.js"));
 
-app.use('/api/users', require('./routes/users.js'))
-
-app.listen(port, host, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+console.log(`Example app listening at http://localhost:${port}`);
